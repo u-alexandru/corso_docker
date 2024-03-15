9595313a573697c5099e1f266831fc856579cf7d
Alexandru Ungureanu <u95.alexandru@gmail.com>
2024-03-15 13:00:24 +0100

    Added lesson3

--- examples/NodeServer/index.js
+++ examples/NodeServer/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const env = require('process').env;
 const port = env.PORT || 3000;
+const myString = env.aString || 'default value';
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`Hello World! ${myString}`)
 })
 
 app.listen(port, () => {
